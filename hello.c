85c1d072d61b8e5786f6f00c0afab585f6e4a0df
Your Name <halyassiri2@huskers.edu.unl>
2024-08-29 12:41:06 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Hassanen Alyassiri
+ * Date: 8/27/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hassanen Alyassiri\n");
+
+  return 0;
+}
